8b6edcfd68e95e5a2f3b01f1a4ee5e894ad62bd9
walk8243 <walk8243@gmail.com>
2024-05-18 22:28:11 +0900

    union and symbol

--- enum/nodejs/src/index.ts
+++ enum/nodejs/src/index.ts
@@ -1,5 +1,10 @@
-import { Position, toJapanese } from './position';
+import * as position from './position';
+import * as yesno from './yesno';
+
+const { Position } = position;
+const { YesNo } = yesno;
 
 (() => {
-	console.log('toJapanese:', toJapanese(Position.TOP));
+	console.log('Position:', position.toJapanese(Position.TOP));
+	console.log('YesNo:', yesno.toJapanese(YesNo.yes));
 })();
