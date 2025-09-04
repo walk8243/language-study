82607bbe2c23cbf44abbd6151f22688bf46c1554
walk8243 <walk8243@gmail.com>
2025-09-05 01:14:47 +0900

    検証用のControllerを作成

---
+++ reactor/src/main/java/xyz/walk8243/study/reactor/TestController.java
@@ -0,0 +1,13 @@
+package xyz.walk8243.study.reactor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public Flux<String> test() {
+        return Flux.just("Controller動作確認用");
+    }
+}
