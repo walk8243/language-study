24dacc58eb8dda974b60853f1a0fe3ef62da51d1
walk8243 <walk8243@gmail.com>
2025-09-05 01:07:35 +0900

    spring inilizr

---
+++ reactor/src/main/java/xyz/walk8243/study/reactor/ReactorApplication.java
@@ -0,0 +1,13 @@
+package xyz.walk8243.study.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorApplication.class, args);
+	}
+
+}
